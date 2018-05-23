f61ca60b3a2188e3bbda5b358cdde066e3cc17d4
L <luciano.e.laratelli@gmail.com>
2018-05-23 15:41:17 -0400

    make using get_TCF not awful

---
+++ waterEquil/process_data.py
@@ -0,0 +1,34 @@
+import sys
+from subprocess import call
+import numpy.fft as npft
+import matplotlib.pyplot as plt
+
+
+def main():
+    if(sys.argv[1] == None):
+        print "Needs exactly one file name as input."
+        sys.exit(1)
+    with open(sys.argv[1], 'r') as program:
+        data = program.readlines()
+
+    col_num = len(data[0].split())
+
+
+    if(col_num != 4):
+        with open(sys.argv[1], 'w') as program:
+            for (number, line) in enumerate(data):
+                program.write('%d  %s' % (number + 1, line))
+    
+
+    call(["./get_TCF", sys.argv[1]])
+
+    correlated_file = sys.argv[1] + ".CORRELATED"
+
+    with open(correlated_file, 'r') as corr:
+        correlated = corr.readlines()
+
+    print npft.fft(correlated)
+
+
+if __name__ == "__main__":
+    main()
